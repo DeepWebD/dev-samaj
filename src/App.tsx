752cad90c0358ac87efee433d993180a614057b4
DeepWebD <deepak.thewebdeveloper@gmail.com>
2024-11-22 16:27:09 +0530

    added header

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import Slider from "./components/Carousel/carousel";
 import Header from "./components/Header/Header";
 function App() {
   return (
-    <div>
+    <div style={{position:'relative'}}>
       <Header />
       <Slider />
     </div>
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,35 +1,24 @@
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import NavDropdown from "react-bootstrap/NavDropdown";
-
+import styled from './Header.module.css'
 function Header() {
   return (
-      <Navbar expand="lg" className="bg-body-tertiary" style={{position:'relative',zIndex:'100', }}>
+    <div style={{position:'absolute',width:'100%',zIndex:'3'}}>
+        <Navbar expand="md" >
         <Container>
-          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+          <Navbar.Brand href="#home" className={styled.link}>React-Bootstrap</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#link">Link</Nav.Link>
-              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">
-                  Another action
-                </NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">
-                  Something
-                </NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">
-                  Separated link
-                </NavDropdown.Item>
-              </NavDropdown>
+              <Nav.Link href="#home" className={styled.link}>Home</Nav.Link>
+              <Nav.Link href="#link" className={styled.link}>Link</Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
       </Navbar>
+    </div>
+      
   );
 }
 
