942562d75d6b4047eb337b1a6f8296fa328d05b1
DeepWebD <deepak.thewebdeveloper@gmail.com>
2024-11-22 19:38:23 +0530

    modified header

--- src/components/Carousel/carousel.tsx
+++ src/components/Carousel/carousel.tsx
@@ -20,7 +20,7 @@ function Slider() {
               objectFit: "cover",
             }}
           />
-          <Carousel.Caption style={{ backgroundColor: "rgba(0,0,0,0.6)" }}>
+          <Carousel.Caption className={styles.caption}>
             <h3>First slide label</h3>
             <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
           </Carousel.Caption>
@@ -30,7 +30,7 @@ function Slider() {
             src={banner2}
             style={{ width: "100vw", height: "100vh", objectFit: "cover" }}
           />
-          <Carousel.Caption style={{ backgroundColor: "rgba(0,0,0,0.6)" }}>
+          <Carousel.Caption className={styles.caption}>
             <h3>Second slide label</h3>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
           </Carousel.Caption>
@@ -40,7 +40,7 @@ function Slider() {
             src={banner3}
             style={{ width: "100vw", height: "100vh", objectFit: "cover" }}
           />
-          <Carousel.Caption style={{ backgroundColor: "rgba(0,0,0,0.6)" }}>
+          <Carousel.Caption className={styles.caption }>
             <h3>Third slide label</h3>
             <p>
               Praesent commodo cursus magna, vel scelerisque nisl consectetur.
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,24 +1,62 @@
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import styled from './Header.module.css'
+import styled from "./Header.module.css";
+
+import Logo from "../../assets/image/logo.png";
 function Header() {
   return (
-    <div style={{position:'absolute',width:'100%',zIndex:'3'}}>
-        <Navbar expand="md" >
-        <Container>
-          <Navbar.Brand href="#home" className={styled.link}>React-Bootstrap</Navbar.Brand>
+    <div style={{ position: "absolute", width: "100%", zIndex: "3", backgroundColor:'rgb(255 69 0 / 60%)' }}>
+      <Navbar expand="lg">
+        <Container style={{maxWidth:'96%'}}>
+          <Navbar.Brand
+            href="#home"
+            className={styled.brand}
+          >
+            <img alt="" src={Logo} className="d-inline-block align-top" />
+            <div className={styled.school}>
+              <div className={styled.name}>Dev Samaj School</div>
+              <div className={styled.location}>Sukhdev Vihar(New Delhi)</div>
+            </div>
+          </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
+          <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
             <Nav className="me-auto">
-              <Nav.Link href="#home" className={styled.link}>Home</Nav.Link>
-              <Nav.Link href="#link" className={styled.link}>Link</Nav.Link>
+              <Nav.Link href="#home" className={styled.link}>
+                ABOUT US
+              </Nav.Link>
+              <Nav.Link href="#link" className={styled.link}>
+                MPD
+              </Nav.Link>
+              <Nav.Link href="#home" className={styled.link}>
+                HOME
+              </Nav.Link>
+              <Nav.Link href="#link" className={styled.link}>
+                PARENTS
+              </Nav.Link>
+              <Nav.Link href="#home" className={styled.link}>
+                CURRICULUM
+              </Nav.Link>
+              <Nav.Link href="#link" className={styled.link}>
+                GALLERY
+              </Nav.Link>
+              <Nav.Link href="#home" className={styled.link}>
+                CONTACT US
+              </Nav.Link>
+              <Nav.Link href="#link" className={styled.link}>
+                REGISTRATION
+              </Nav.Link>
+              <Nav.Link href="#home" className={styled.link}>
+                HISTORY
+              </Nav.Link>
+              <Nav.Link href="#link" className={styled.link}>
+                LOGIN
+              </Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
       </Navbar>
     </div>
-      
   );
 }
 
